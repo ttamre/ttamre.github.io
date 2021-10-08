bf096dd60eceab5afb4096d462c7f15ebec29b76
Tem Tamre <ttamre@ualberta.ca>
2021-10-08 15:14:58 -0600

    finished lab

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,6 +10,7 @@ PATH = 'content'
 TIMEZONE = 'America/Edmonton'
 
 DEFAULT_LANG = 'en'
+THEME = 'templates/mytheme'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
